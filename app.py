c1641884e8d2c8012cff9234ee7bdb81abd6123a
kny <skaduf73@gmail.com>
2021-06-09 16:56:27 +0900

    디비연결 진자템플릿 사용

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask,render_template ,request
+from werkzeug.utils import redirect
+import ddb
+from flask import Flask,render_template ,request, redirect, session, sessions
 from hello import money_pung,idpw_check
 app = Flask(__name__)
+
+#로그인 처리(세션에 필요)
+app.secret_key = b'aaa!111/' ## 임의로 이름 설정
 
 @app.route('/')
 def hello():
@@ -14,7 +19,11 @@ def signup():
         email = request.form['email']
         pswd = request.form['pswd']
         username = request.form['username']
-        return "{}</br>{}</br>{}</br> 회원정보".format(email,pswd,username)
+        # 위 데이터들을 어딘가에 저장해서 로그인할때
+        # 사용해야하는데...
+        ddb.insert_member(email, pswd, username)
+        return redirect('/') ## 메인으로 보내기
+        #return "{}</br>{}</br>{}</br> 회원정보".format(email,pswd,username)
 
 @app.route('/signin', methods = ['GET' , 'POST'])
 def signin():
@@ -24,12 +33,28 @@ def signin():
         email = request.form['email']
         pswd = request.form['pswd']
         #이메일 패스워드 체크
-        msg = idpw_check(email,pswd)
-        return "{}</br>{}</br>{}</br>".format(email,pswd,msg)
+        ## msg = idpw_check(email,pswd)
+        ret = ddb.select_member(email, pswd)
+        if ret != None :
+            session['email'] = email
+            return redirect('/')
+        else :
+            return redirect('/signin')
+        # return "{}</br>{}</br>{}</br>".format(email,pswd,msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect('/') # 메인으로
 
 @app.route('/money')
 def money():
-    return render_template("money.html")
+    # 로그인된 사용자들만 돈뻥 페이지가 보이게
+    # 세션 체크
+    if 'email' in session:
+        return render_template("money.html")
+    else:
+        return redirect('/signin')
 
 @app.route('/show', methods=['GET', 'POST'])
 def show():
@@ -41,12 +66,14 @@ def show():
         #money 의 값이 문자열 이기 때문에 숫자로 변경
         print("전달된 값은? ",int(money)*3)
         im = money_pung(int(money)) #뻥튀기 함수 사용
-        return "당신의 능력을 보여줘~ 얍<br><b>{}</b>원 드립니다".format(im)
+        return render_template("show.html",money=im)
+        #return "당신의 능력을 보여줘~ 얍<br><b>{}</b>원 드립니다".format(im)
         #return "당신의 능력을 보여줘~ 얍<br><b>{}</b>원 드립니다".format(money_pung(int(money)))
 
 @app.route('/showmoney')
 def showmoney():
     return render_template("smoney.html")
+
 @app.route('/hello/')
 def hellohtml():
     return render_template("hello.html")
